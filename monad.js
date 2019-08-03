be3dcefc24f37bf14c276899d22be806ce420471
sean.shen@perficient.com <sean.shen@perficient.com>
2019-08-03 23:59:06 +0800

    support.js 文件中提供同步的容器，所以引用时改名为syncsupprt

--- monad.js
+++ monad.js
@@ -1,4 +1,4 @@
-var support = require('./support')
+var syncSupport = require('./support')
 var Task = require('data.task')
 var fs = require('fs');
 
@@ -25,35 +25,35 @@ const user = {
 
 var safeProp = prop => json => {
     if(prop == null || prop == undefined){
-        return support.Left.of('The prop is required.');
+        return syncSupport.Left.of('The prop is required.');
     }
     if(json[prop]){
-        return support.Right.of(json[prop])
+        return syncSupport.Right.of(json[prop])
     }
     else{
-        return support.Left.of(`The ${prop} property does not exist in the provided object` )
+        return syncSupport.Left.of(`The ${prop} property does not exist in the provided object` )
     }
 }
 
 var join = container => container.__value;
 
-var getAddress = support.compose(join,support.map(safeProp('street')),safeProp('address'));
-var getNumber = support.compose(join,support.map(safeProp('number')),getAddress)
+var getAddress = syncSupport.compose(join,syncSupport.map(safeProp('street')),safeProp('address'));
+var getNumber = syncSupport.compose(join,syncSupport.map(safeProp('number')),getAddress)
 var chain = f => obj => obj.map(f).join()
 // safeProp('address')(user)
 //         .chain(safeProp('street'))
 //         .chain(safeProp('number'))
-//         .chain(support.log) 
-var safePropIO = prop => json => new support.IO(() => support.Maybe.of(json[prop]));
+//         .chain(syncSupport.log) 
+var safePropIO = prop => json => new syncSupport.IO(() => syncSupport.Maybe.of(json[prop]));
 
 //getfile ::　string -> IO String
-var getFile = filename => new support.IO(() => fs.readFileSync(filename))
+var getFile = filename => new syncSupport.IO(() => fs.readFileSync(filename))
 var userAddressNumber = getFile('./test.json')
                             .map(JSON.parse)
                             .chain(safePropIO('address'))
-                            .chain(support.chain(safePropIO('street')))
-                            .chain(support.chain(safePropIO('number1')))
-                            .map(support.log)
+                            .chain(syncSupport.chain(safePropIO('street')))
+                            .chain(syncSupport.chain(safePropIO('number1')))
+                            .map(syncSupport.log)
                             .__value()
                             .__value
 console.log(userAddressNumber)
